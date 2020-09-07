425ced3bb2e2056fc97408b617d2155da611d771
kko0831 <kko0831@hanmail.net>
2020-09-07 20:19:23 +0900

    #11.1 AuthNavigation

--- components/NavController.js
+++ components/NavController.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 import { useIsLoggedIn, useLogIn, useLogOut } from "../AuthContext";
+import AuthNavigation from "../navigation/AuthNavigation";
 
 export default () => {
   const isLoggedIn = useIsLoggedIn();
@@ -8,15 +9,13 @@ export default () => {
   const logOut = useLogOut();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1 }}>
       {isLoggedIn ? (
         <TouchableOpacity onPress={logOut}>
           <Text>Log Out</Text>
         </TouchableOpacity>
       ) : (
-        <TouchableOpacity onPress={logIn}>
-          <Text>Log in</Text>
-        </TouchableOpacity>
+        <AuthNavigation />
       )}
     </View>
   );
---
+++ navigation/AuthNavigation.js
@@ -0,0 +1,20 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import AuthHome from "../screens/Auth/AuthHome";
+import Signup from "../screens/Auth/Signup";
+import Login from "../screens/Auth/Login";
+import Confirm from "../screens/Auth/Signup";
+
+const AuthNavigation = createStackNavigator(
+  {
+    AuthHome,
+    Signup,
+    Login,
+    Confirm,
+  },
+  {
+    headerMode: "none",
+  }
+);
+
+export default createAppContainer(AuthNavigation);
