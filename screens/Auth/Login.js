827bcc5a21b7d4a08c4ba0d129c896f5200bdbfa
kko0831 <kko0831@hanmail.net>
2020-09-10 20:29:05 +0900

    #12.4 Login part Two

--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { TouchableWithoutFeedback, Keyboard } from "react-native";
+import { TouchableWithoutFeedback, Keyboard, Alert } from "react-native";
 import AuthButton from "../../components/AuthButton";
 import AuthInput from "../../components/AuthInput";
 import useInput from "../../hooks/useInput";
-import { Alert } from "react-native";
+import { useMutation } from "@apollo/react-hooks";
+import { LOG_IN } from "./AuthQueries";
 
 const View = styled.View`
   justify-content: center;
@@ -12,9 +13,15 @@ const View = styled.View`
   flex: 1;
 `;
 
-export default () => {
+export default ({ navigation }) => {
   const emailInput = useInput("");
-  const handleLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const requestSecret = useMutation(LOG_IN, {
+    variables: {
+      email: emailInput.value,
+    },
+  });
+  const handleLogin = async () => {
     const { value } = emailInput;
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (value === "") {
@@ -23,6 +30,16 @@ export default () => {
       return Alert.alert("Please write an email");
     } else if (!emailRegex.test(value)) {
       return Alert.alert("That email is invalid");
+    }
+    try {
+      setLoading(true);
+      await requestSecret();
+      Alert.alert("Check your email");
+      navigation.navigate("Confirm");
+    } catch (e) {
+      Alert.alert("Can't log in now");
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -36,7 +53,7 @@ export default () => {
           onEndEditing={handleLogin}
           autoCorrect={false}
         />
-        <AuthButton onPress={handleLogin} text="Log In" />
+        <AuthButton loading={loading} onPress={handleLogin} text="Log In" />
       </View>
     </TouchableWithoutFeedback>
   );
