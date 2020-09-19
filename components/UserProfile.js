2d5f5834926385659f3c407d0f5a46a1d818d33c
kko0831 <kko0831@hanmail.net>
2020-09-20 01:22:50 +0900

    #17.2 UserProfile Component Styles

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,8 +1,115 @@
 import React from "react";
+import { Image, View, TouchableOpacity, Platform } from "react-native";
 import styled from "styled-components";
+import { Ionicons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
+import theme from "../theme";
+import constants from "../constants";
 
-const UserProfile = ({}) => null;
+// 프로필 Header
+const ProfileHeader = styled.View`
+  padding: 20px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+const HeaderColumn = styled.View``;
+
+const ProfileStats = styled.View`
+  flex-direction: row;
+`;
+
+const Stat = styled.View`
+  align-items: center;
+  margin-left: 40px;
+`;
+
+const Bold = styled.Text`
+  font-weight: bold;
+`;
+
+const StatName = styled.Text`
+  margin-top: 5px;
+  font-size: 12px;
+  color: ${theme.darkGreyColor};
+`;
+
+// 프로필 fullName
+const ProfileMeta = styled.View`
+  margin-top: 10px;
+  padding-horizontal: 20px;
+`;
+
+const Bio = styled.Text``;
+
+// 프로필 footer
+const ButtonContainer = styled.View`
+  flex-direction: row;
+  margin-top: 30px;
+`;
+
+const Button = styled.View`
+  width: ${constants.width / 2};
+  align-items: center;
+`;
+
+const UserProfile = ({
+  avatar,
+  postsCount,
+  followersCount,
+  followingCount,
+  bio,
+  fullName,
+}) => {
+  return (
+    <View>
+      <ProfileHeader>
+        <Image
+          style={{ height: 80, width: 80, borderRadius: 40 }}
+          source={{ uri: avatar }}
+        />
+        <HeaderColumn>
+          <ProfileStats>
+            <Stat>
+              <Bold>{postsCount}</Bold>
+              <StatName>Posts</StatName>
+            </Stat>
+            <Stat>
+              <Bold>{followersCount}</Bold>
+              <StatName>Followers</StatName>
+            </Stat>
+            <Stat>
+              <Bold>{followingCount}</Bold>
+              <StatName>Following</StatName>
+            </Stat>
+          </ProfileStats>
+        </HeaderColumn>
+      </ProfileHeader>
+      <ProfileMeta>
+        <Bold>{fullName}</Bold>
+        <Bio>{bio}</Bio>
+      </ProfileMeta>
+      <ButtonContainer>
+        <TouchableOpacity>
+          <Button>
+            <Ionicons
+              size={32}
+              name={Platform.OS === "ios" ? "ios-grid" : "md-grid"}
+            />
+          </Button>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Button>
+            <Ionicons
+              size={32}
+              name={Platform.OS === "ios" ? "ios-list" : "md-list"}
+            />
+          </Button>
+        </TouchableOpacity>
+      </ButtonContainer>
+    </View>
+  );
+};
 
 UserProfile.propTypes = {
   id: PropTypes.string.isRequired,
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -30,9 +30,9 @@ const stackFactory = (initialRoute, customConfig) =>
       },
       UserDetail: {
         screen: UserDetail,
-        navigationOptions: {
-          title: "User",
-        },
+        navigationOptions: ({ navigation }) => ({
+          title: navigation.getParam("username"),
+        }),
       },
     },
     {
